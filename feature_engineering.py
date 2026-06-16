5af92459689ff0014f2ad534c4625a33cb66b361
Leo <leoyang388@gmail.com>
2026-06-16 15:07:42 -0400

    finished random forest model

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,24 +1,28 @@
-FEATURES = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+import numpy as np
 
 
-def preprocess_data(train_data, test_data):
-    train_data = train_data.copy()
-    test_data = test_data.copy()
+FEATURES = [
+    "Pclass",
+    "Sex",
+    "Age",
+    "Fare",
+    "Embarked",
+    "FamilySize",
+    "IsAlone",
+    "HasCabin",
+    "Title",
+]
 
-    age_median = train_data["Age"].median()
-    fare_median = train_data["Fare"].median()
-    embarked_mode = train_data["Embarked"].mode()[0]
 
-    for data in (train_data, test_data):
-        data.fillna(
-            {
-                "Age": age_median,
-                "Fare": fare_median,
-                "Embarked": embarked_mode,
-            },
-            inplace=True,
-        )
-        data["Sex"] = data["Sex"].map({"male": 0, "female": 1})
-        data["Embarked"] = data["Embarked"].map({"C": 0, "Q": 1, "S": 2})
+def preprocess_data(data):
+    data = data.copy()
 
-    return train_data, test_data
+    data["FamilySize"] = data["SibSp"] + data["Parch"] + 1
+    data["IsAlone"] = np.where(data["FamilySize"] == 1, 1, 0)
+    data["HasCabin"] = np.where(data["Cabin"].isna(), 0, 1)
+
+    data["Title"] = data["Name"].str.extract(r",\s*([^.]*)\.", expand=False)
+    common_titles = {"Mr", "Miss", "Mrs", "Master"}
+    data["Title"] = np.where(data["Title"].isin(common_titles), data["Title"], "Rare")
+
+    return data
--- model.py
+++ model.py
@@ -1,31 +1,66 @@
 import pandas as pd
+from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
 
 from feature_engineering import FEATURES, preprocess_data
 
 
 HYPERPARAMETER_GRID = {
-    "n_estimators": [200, 400, 800, 1600],
-    "max_depth": [None, 4, 6, 10],
-    "min_samples_leaf": [1, 2, 4, 8],
-    "max_features": ["sqrt", "log2"],
+    "classifier__n_estimators": [150, 300, 600, 900],
+    "classifier__max_depth": [4, 6, 8, None],
+    "classifier__min_samples_leaf": [1, 2, 4],
+    "classifier__max_features": ["sqrt", "log2"],
 }
+
+NUMERIC_FEATURES = ["Pclass", "Age", "Fare", "FamilySize", "IsAlone", "HasCabin"]
+CATEGORICAL_FEATURES = ["Sex", "Embarked", "Title"]
 
 
 def main():
     train_data = pd.read_csv("Dataset/train.csv")
     test_data = pd.read_csv("Dataset/test.csv")
-    train_data, test_data = preprocess_data(train_data, test_data)
+    train_data = preprocess_data(train_data)
+    test_data = preprocess_data(test_data)
 
     train_X = train_data[FEATURES]
     train_y = train_data["Survived"]
     test_X = test_data[FEATURES]
 
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("numeric", SimpleImputer(strategy="median"), NUMERIC_FEATURES),
+            (
+                "categorical",
+                Pipeline(
+                    steps=[
+                        ("imputer", SimpleImputer(strategy="most_frequent")),
+                        (
+                            "encoder",
+                            OneHotEncoder(handle_unknown="ignore", sparse_output=False),
+                        ),
+                    ]
+                ),
+                CATEGORICAL_FEATURES,
+            ),
+        ]
+    )
+    model = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("classifier", RandomForestClassifier(random_state=42)),
+        ]
+    )
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
     grid_search = GridSearchCV(
-        RandomForestClassifier(random_state=42),
+        model,
         HYPERPARAMETER_GRID,
         scoring="accuracy",
+        cv=cv,
         n_jobs=-1,
     )
     grid_search.fit(train_X, train_y)
